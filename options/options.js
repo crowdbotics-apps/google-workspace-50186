644f684331b3891087fb7a68f5909b5c89ed002f
Crowdbotics <team@crowdbotics.com>
2024-11-30 19:10:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "google-workspace-50186",
+  url: "https://google-workspace-50186.botics.co",
+  api: "https://google-workspace-50186.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
